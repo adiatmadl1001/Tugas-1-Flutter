2cc752013958a44d10e9ac94a05cd26841640eef
ADIATMA DEWA LAKSANA <adiatmalaksana30@gmail.com>
2024-10-30 18:08:49 +0700

    added TitleSection

--- lib/TitleSection.dart
+++ lib/TitleSection.dart
@@ -1,33 +1,3 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    const String appTitle = 'Flutter layout demo';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: const SingleChildScrollView(
-          child: Column(
-            children: [
-              TitleSection(
-                name:'Kriz Kruz Krax',
-                location : 'Kediri',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
 class TitleSection extends StatelessWidget {
   const TitleSection({
     super.key,
