1cdeac856e45b98eb1bb95ae4f47aa4c85ee3644
ADIATMA DEWA LAKSANA <adiatmalaksana30@gmail.com>
2024-11-02 10:26:17 +0700

    added button

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,10 @@ class MyApp extends StatelessWidget {
           child: Column(
             children: [
               TitleSection(
-                name:'Kriz Kruz Krax',
-                location : 'Kediri',
+                name: 'Kriz Kruz Krax',
+                location: 'Kediri',
               ),
+              ButtonSection(),
             ],
           ),
         ),
@@ -28,6 +29,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class TitleSection extends StatelessWidget {
   const TitleSection({
     super.key,
@@ -76,6 +78,72 @@ class TitleSection extends StatelessWidget {
           const Text('41'),
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: 'CALL',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
